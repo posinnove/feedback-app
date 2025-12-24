f1633a90900e29ff0a4d69c61e63c935dcb154c3
ndizeyedavid <davidndizeye101@gmail.com>
2025-12-24 23:45:27 +0200

    installed prettier to resolve formating issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ function App() {
     <>
       {/* <p className="text-text">mellow</p> */}
       {/* <button className="btn btn-primary">mellow</button> */}
-    <section className="flex items-center justify-center w-full h-dvh">
-      <div className="card flex flex-col items-center justify-center gap-4">
-        <h2 className="text-5xl font-semibold">Voxela</h2>
-        <p>Share your feedback, shape the future</p>
-        <button className="btn btn-primary">Get Started</button>
-      </div>
-    </section>
+      <section className="flex items-center justify-center w-full h-dvh">
+        <div className="card flex flex-col items-center justify-center gap-4">
+          <h2 className="text-5xl font-semibold">Voxela</h2>
+          <p>Share your feedback, shape the future</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </section>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
