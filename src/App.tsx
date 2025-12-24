d78ab5de1a9e943f9df4c0a4e58af6b8fcdc1059
ndizeyedavid <davidndizeye101@gmail.com>
2025-12-24 23:33:16 +0200

    upd: finilized tailwind uitility classes for reusablitiy and consistency

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ function App() {
   return (
     <>
       {/* <p className="text-text">mellow</p> */}
-      <button className="btn">mellow</button>
+      {/* <button className="btn btn-primary">mellow</button> */}
+      <div className="card w-fit">
+        <h2 className="text-lg font-bold">Mellow</h2>
+      </div>
     </>
   );
 }
